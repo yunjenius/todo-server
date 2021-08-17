2fe385e215cac7a9a02297d921f30dc0786b9022
yunjin78 <whyj97@gmail.com>
2021-08-17 23:29:04 +0900

    Initial commit

---
+++ src/main/java/org/example/TodoServerApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.repository.TodoRepository;
+
+public class TodoServerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO TODO!");
+
+        TodoRepository repository;
+
+    }
+}
---
+++ src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,28 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(name = "todoOrder", nullable = false) //order는 db예약어로 사용 중이므로 name 지정
+    private Long order;
+    
+    @Column(nullable = false)
+    private Boolean completed;
+
+}
