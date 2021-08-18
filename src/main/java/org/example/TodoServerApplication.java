d6ce955b8602623a4ff14d95d0fa90fef671fdd8
yunjin78 <whyj97@gmail.com>
2021-08-19 00:09:58 +0900

    todo-server API 완료

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.repository.TodoRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("HELLO TODO!");
-
-        TodoRepository repository;
+        SpringApplication.run(TodoServerApplication.class, args);
 
     }
 }
